b495894259be0048b0a3d6f2f3dad4c7f3928331
James-yaoshenglong <yaoshenglong758063348@gmail.com>
2025-09-15 03:04:02 +0000

    add support for print retrun value

--- drltrace_src/drltrace.cpp
+++ drltrace_src/drltrace.cpp
@@ -361,6 +361,9 @@ lib_entry(void *wrapcxt, INOUT void **user_data)
      */
     print_symbolic_args(name, wrapcxt, func);
 
+    /* Set up post-call wrapper to capture return value */
+    drwrap_set_post_call(wrapcxt, lib_exit, *user_data);
+
     if (op_print_ret_addr.get_value()) {
         ret_addr = drwrap_get_retaddr(wrapcxt);
         res = drmodtrack_lookup(drcontext, ret_addr, &mod_id, &mod_start);
@@ -374,6 +377,49 @@ lib_entry(void *wrapcxt, INOUT void **user_data)
     dr_fprintf(outf, "\n");
     if (mod != NULL)
         dr_free_module_data(mod);
+}
+
+/****************************************************************************
+ * Library exit wrapping
+ */
+
+static void
+lib_exit(void *wrapcxt, void *user_data)
+{
+    const char *name = (const char *) user_data;
+    ptr_uint_t retval = (ptr_uint_t)drwrap_get_retval(wrapcxt);
+    void *drcontext = drwrap_get_drcontext(wrapcxt);
+    thread_id_t tid = dr_get_thread_id(drcontext);
+    
+    // Create a fake argument structure for the return value
+    drltrace_arg_t ret_arg;
+    memset(&ret_arg, 0, sizeof(ret_arg));
+    ret_arg.value = retval;
+    ret_arg.value64 = (uint64)retval;
+    ret_arg.ordinal = -1;  // Special ordinal for return value
+    ret_arg.mode = DRSYS_PARAM_RETVAL | DRSYS_PARAM_INLINED;
+    ret_arg.pre = false;
+    ret_arg.arg_name = "retval";
+    ret_arg.size = sizeof(ptr_uint_t);
+    ret_arg.reg = DR_REG_NULL;
+    
+    // Treat all return values as void type
+    ret_arg.type = DRSYS_TYPE_VOID;
+    ret_arg.type_name = "void";
+    
+    // Print thread ID and function name
+    if (tid != INVALID_THREAD_ID)
+        dr_fprintf(outf, "~~%d~~ ", tid);
+    else
+        dr_fprintf(outf, "~~Dr.L~~ ");
+    
+    // Print function name and "returns:"
+    dr_fprintf(outf, "%s returns", name);
+    
+    // Reuse the existing print_arg function
+    print_arg(drcontext, &ret_arg);
+    
+    dr_fprintf(outf, "\n");
 }
 
 static void
@@ -405,13 +451,13 @@ iterate_exports(const module_data_t *info, bool add)
         if (func != NULL) {
             if (add) {
                 IF_DEBUG(bool ok =)
-                    drwrap_wrap_ex(func, lib_entry, NULL, (void *) sym->name, 0);
+                    drwrap_wrap_ex(func, lib_entry, lib_exit, (void *) sym->name, 0);
                 ASSERT(ok, "wrap request failed");
                 VNOTIFY(2, "wrapping export %s!%s @" PFX NL,
                        dr_module_preferred_name(info), sym->name, func);
             } else {
                 IF_DEBUG(bool ok =)
-                    drwrap_unwrap(func, lib_entry, NULL);
+                    drwrap_unwrap(func, lib_entry, lib_exit);
                 ASSERT(ok, "unwrap request failed");
             }
         }
